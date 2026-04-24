8b00b5f95dbe9d790b7e88b46864ab5388a12691
Sina Sadeghi <sadeq.s502@gmail.com>
2026-04-24 15:09:27 +0200

    initilialized implementation of methods in python.

---
+++ py/euclid_distance/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numba import jit, prange
+import time
+
+# create two large arrays of double with the size of 1 million
+size = 1_000_000
+a = np.random.rand(size).astype(np.float64)
+b = np.random.rand(size).astype(np.float64)
+
+@njit(parallel=True, fastmath=True)
+def numba_euclidean_distance(a, b):
+    n = a.size
+    sum = 0.0
+    for i in prange(n):
+        d = a[i] - b[i]
+        sum += d * d
+    return np.sqrt(sum)
+
+def np_euclidean_distance(a, b):
+    return np.linalg.norm(a - b)**2
+
+def euclidean_distance(a, b):
+    sum = 0.0
+    for i in range(a.size):
+        d = a[i] - b[i]
+        sum += d * d
+    return np.sqrt(sum)
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+    dist_numba = numba_euclidean_distance(a, b)
+    end_time = time.time()
+    print(f"Numba distance: {dist_numba:.6f}, Time taken: {end_time - start_time:.4f} seconds")
+
+    start_time = time.time()
+    dist_numpy = np_euclidean_distance(a, b)
+    end_time = time.time()
+    print(f"NumPy distance: {dist_numpy:.6f}, Time taken: {end_time - start_time:.4f} seconds")
+
+    start_time = time.time()
+    dist_standard = euclidean_distance(a, b)
+    end_time = time.time()
+    print(f"Standard distance: {dist_standard:.6f}, Time taken: {end_time - start_time:.4f} seconds")
